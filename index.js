82e2d9991974ae2f8bb9ab7e5c494b4118f9d609
Alymourady <aly@scitecs.com>
2025-05-04 09:51:13 +0300

    index.js

--- index.js
+++ index.js
@@ -1,16 +1,66 @@
-const express = require('express');
-const axios = require('axios');
-const app = express();
 
-app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+const HUBSPOT_API_URL = 'https://api.hubapi.com/crm/v3/objects';
+const CUSTOM_OBJECT = '2-44110741'; 
+const ACCESS_TOKEN = process.env.HUBSPOT_TOKEN;
+console.log("TOKEN CHECK:", ACCESS_TOKEN);
+// GET homepage
+app.get('/', async (req, res) => {
+    try {
+      const response = await axios.get(`${HUBSPOT_API_URL}/${CUSTOM_OBJECT}`, {
+        headers: { Authorization: `Bearer ${ACCESS_TOKEN}` }
+      });
+      const records = response.data.results;
+      res.render('homepage', { title: 'Home', records });
+    } catch (err) {
+      console.error("Error fetching custom object:", err.response?.data || err.message);
+      res.status(500).send('Error loading records.');
+    }
+  });
+  
+
+// GET form
+app.get('/update-cobj', (req, res) => {
+  res.render('updates', { title: 'Update Custom Object Form' });
+});
+
+// POST form
+app.post('/update-cobj', async (req, res) => {
+  const { name, kaza_2, kaza_1 } = req.body;
+
+  try {
+    await axios.post(`${HUBSPOT_API_URL}/${CUSTOM_OBJECT}`, {
+      properties: {
+        name,
+        kaza_2,
+        kaza_1
+      }
+    }, {
+      headers: { Authorization: `Bearer ${ACCESS_TOKEN}` }
+    });
+
+    res.redirect('/');
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Error creating record.');
+  }
+});
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
 
 // * Code for Route 1 goes here
 
@@ -67,5 +117,3 @@ app.post('/update', async (req, res) => {
 */
 
 
-// * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
